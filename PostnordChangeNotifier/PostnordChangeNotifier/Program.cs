92733b9e6613f820132b152434e414d0ed82be8b
Kristoffer <fobber1995@gmail.com>
2018-09-03 14:52:04 +0200

    Add method to get tracking id from user input

---
+++ PostnordChangeNotifier/PostnordChangeNotifier/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace PostnordChangeNotifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var trackingId = GetTrackingIdFromUser();
+            Console.WriteLine("Good");
+        }
+
+        private static string GetTrackingIdFromUser()
+        {
+            Console.Title = "Please enter tracking ID";
+            string input = "";
+            while (!Regex.IsMatch(input, "^[0-9]{10,20}$"))
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("Please enter tracking ID: ");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                input = Console.ReadLine();
+                Console.Clear();
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            return input;
+        }
+    }
+}
